e04d1cf0106218b98f9eb919a29e137f71ab3c18
saba1122333 <tchumburidzesaba@gmail.com>
2025-04-13 15:38:03 +0400

    init PGNParser class and it's methods

---
+++ src/PGNParser.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PGNParser {
+    public Map<String, String> HeaderParser(StringBuilder headerSection) {
+        return new HashMap<>();
+    }
+
+    public List<String> MovesParser(StringBuilder moveSection) {
+        return new ArrayList<>();
+    }
+    public void PGNFileParser(String FileName) { }
+
+    }
+
+
+
+
+
