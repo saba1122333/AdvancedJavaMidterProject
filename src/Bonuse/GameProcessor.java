f6c5dec4acb3413b7f08682b139256a90ffcd295
Saba Tchumburidze <102433145+saba1122333@users.noreply.github.com>
2025-04-19 04:26:18 +0400

    Update GameProcessor.java

--- src/Bonuse/GameProcessor.java
+++ src/Bonuse/GameProcessor.java
@@ -16,9 +16,8 @@ public class GameProcessor {
     public static void processGames(List<String> filePaths, int nThreads)
             throws InterruptedException, ExecutionException {
 
-        // 1) Create a fixed pool of N threads
         ExecutorService parsersPoolService = Executors.newFixedThreadPool(nThreads);
-        //
+                
         List<Future<List<List<String>>>> allGames = new ArrayList<>();
         for (String filePath : filePaths) {
             allGames.add(parsersPoolService.submit(() -> {
@@ -27,10 +26,12 @@ public class GameProcessor {
                 return p.getGameList();
             }));
         }
+            
         List<List<String>> allFlattenedGames = new ArrayList<>();
         for (Future<List<List<String>>> file : allGames) {
             allFlattenedGames.addAll(file.get());
         }
+                
         List<Future<List<ChessMove>>> chessMoves = new ArrayList<>();
         for (List<String> game : allFlattenedGames) {
             chessMoves.add(
@@ -38,6 +39,7 @@ public class GameProcessor {
                             ChessMovesParser.parse(game)
                     ));
         }
+                
         parsersPoolService.shutdown();
 
         GameMasterController gm = new GameMasterController(new ChessBoard(), true);
